eb70402b49fa7dd7df3a2be91999332111ae4103
swathi <rachamalla.swathi3@gmail.com>
2022-12-18 12:36:09 +0530

    added

--- First_Script.py
+++ First_Script.py
@@ -3,7 +3,7 @@ from datetime import datetime,timedelta
 import os
 import pytz
 print("Executing First script")
-g = Github("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")
+g = Github("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")
 repo = g.get_repo("swathirachamalla/CiCd-Learning")
 Branch= repo.get_branch("Development")
 sha =Branch.commit.sha  
@@ -11,6 +11,7 @@ commit = repo.get_commit(sha=sha)
 start_time = commit.commit.author.date
 end_time =datetime.now(pytz.timezone('GMT'))
 c = datetime(end_time.year,end_time.month,end_time.day,end_time.hour,end_time.minute,end_time.second)- datetime(start_time.year,start_time.month,start_time.day,start_time.hour,start_time.minute,start_time.second)
+print("c")
 if c > timedelta(minutes=5):
     print("Executing Script.sh")
     os.system("script.sh")
